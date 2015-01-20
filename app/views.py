4a93aa72c716b5c951601445855a15b8bbaf606d
Ben Everson <ben.everson@gmail.com>
2015-01-20 11:04:00 -0800

    working through flask tutorial

--- app/views.py
+++ app/views.py
@@ -4,5 +4,14 @@ from app import app
 @app.route('/')
 @app.route('/index')
 def index():
-    user = {'nickname': 'Miguel'} # fake user
-    return render_template("index.html", title = 'Home', user = user)
+    user = {'nickname': 'Miguel'}  # fake user
+    return '''
+<html>
+  <head>
+    <title>Home Page</title>
+  </head>
+  <body>
+    <h1>Hello, ''' + user['nickname'] + '''</h1>
+  </body>
+</html>
+'''
