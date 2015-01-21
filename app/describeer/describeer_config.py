6532a9299c7a0cbcf6ab3bf903d3097c7407644a
Ben Everson <ben.everson@gmail.com>
2015-01-21 15:32:17 -0800

    building heuristic scoring for beers in search_beers function of model.py

--- app/describeer/describeer_config.py
+++ app/describeer/describeer_config.py
@@ -1,5 +1,13 @@
 DEFAULT_MODEL_PATH = 'ReviewIndividualSentencesMultiLabelSize300.doc2vec'
 DEFAULT_CSV_PATH = 'filtered_raw_reviews.csv'
+N_SEARCH_RESULTS = 50
 
 style_column = 5
 name_column = 4
+overall_score_column = 8
+
+# weights for overall scoring of beers
+STYLE_SIM_WEIGHT = 2.0
+BEER_SIM_WEIGHT = 1.0
+BEER_POP_WEIGHT = 1.5
+BEER_RATING_WEIGHT = 1.5
--- app/describeer/model.py
+++ app/describeer/model.py
@@ -6,16 +6,19 @@ from gensim.models import Doc2Vec
 from collections import defaultdict
 from numpy import argsort
 logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
-from describeer_config import DEFAULT_MODEL_PATH, DEFAULT_CSV_PATH, style_column, name_column
+from describeer_config import DEFAULT_MODEL_PATH, DEFAULT_CSV_PATH, style_column, name_column, rating_column
 
 # variables
 _model = None
 
 # lookups
 _name_to_style = {}
-_name_to_popularity = defaultdict(int)
+_name_to_popularity = {}
+_name_to_rating = {}
+
 _style_list = []
 _name_list = []
+
 
 def tokenize_beer_noun(beer_noun, reverse=False):
 	if reverse: # de-tokenize the term
@@ -24,15 +27,37 @@ def tokenize_beer_noun(beer_noun, reverse=False):
 
 # TODO: add cPickle functionality to speed this up
 def init_lookups():
+
+	# variables
+	global _name_to_style
+	global _name_to_popularity
+	global _name_to_rating
+	global _style_list
+	global _name_list
+
+	_name_to_popularity_not_norm = defaultdict(int)
+	_name_to_rating_not_norm = defaultdict(int)
+	total_reviews = 0 
+	max_reviews_any_beer = 0
+
+	# populate the lookups
 	with open(DEFAULT_CSV_PATH, 'rb') as f:
 		reader = csv.reader(f)
 		next(reader, None) # skip the header
 		for row in reader:
 			beer_name = tokenize_beer_noun(row[name_column])
 			beer_style = tokenize_beer_noun(row[style_column])
+			beer_rating = int(row[rating_column])
 
-			# increment this beer's popularity count
-			_name_to_popularity[beer_name] += 1 
+			# increment this beer's overall popularity
+			_name_to_popularity_not_norm[beer_name] += 1 
+
+			# update the max reviews for any one beer
+			if _name_to_popularity_not_norm[beer_name] > max_reviews_any_beer:
+				max_reviews_any_beer = _name_to_popularity_not_norm[beer_name]
+
+			# increment this beer's overall rating
+			_name_to_rating_not_norm[beer_name] += beer_rating
 
 			# init the other lookups
 			if beer_name not in _name_to_style.keys():
@@ -43,6 +68,16 @@ def init_lookups():
 
 			if beer_style not in _style_list:
 				_style_list.append(beer_style)
+
+			total_reviews += 1 # increment total reviews
+
+	# populate _name_to_rating with average rating per review for each beer name
+	for rated_beer in _name_to_rating_not_norm.keys():
+		_name_to_rating[rated_beer] = (1.0 * _name_to_rating_not_norm[rated_beer])/_name_to_popularity_not_norm[rated_beer]
+
+	# populate _name_to_popularity with (# reviews for this beer)/(max # reviews for any beer)
+	for reviewed_beer in _name_to_popularity_not_norm.keys():
+		_name_to_popularity[reviewed_beer] = (1.0 * _name_to_popularity_not_norm[reviewed_beer])/max_reviews_any_beer
 
 # add/swap additional search terms for every term in lookup_list encountered
 # lookup_list = reference list of terms to replace (ex. beer names)
@@ -62,7 +97,7 @@ def prepare_search_terms(search_terms=[], include_original = True, lookup_list=[
 
 # return list of relevant beers (dicts) for a given positive, negative search query
 # TODO: account for the case where no search terms are included
-def get_relevant_terms(positive=[], negative=[], n_results = 20, use_cosmul = False, returnable_words = []):
+def get_relevant_terms(positive=[], negative=[], use_cosmul = False, returnable_words = []):
 
 	positive_terms = prepare_search_terms(positive)
 	negative_terms = prepare_search_terms(negative)
@@ -88,8 +123,21 @@ def init_model(model_path = DEFAULT_MODEL_PATH):
 
 # parse a search term into positive and negative terms
 # and feed to get_relevant_terms function
-def search_beers(search_term):
-	pass
+def search_beers(positive_search_terms, negative_search_terms, n_results = N_SEARCH_RESULTS):
+
+	# WARNING: assuming positive_search_terms and negative_search_terms are parsed already
+	# TODO: put parsing logic here, using regex
+
+	# get beer relevance scores
+	relevant_beers = get_relevant_terms(positive=positive_search_terms, negative=negative_search_terms, returnable_words=_name_list)
+
+	# get style relevance scores
+	relevant_styles = get_relevant_terms(positive=positive_search_terms, negative=negative_search_terms, returnable_words=_style_list)
+
+	
+
+
+
 
 # initialization
 def initialize():
