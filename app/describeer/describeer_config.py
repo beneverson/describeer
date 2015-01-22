a4d31e7e5557d3b8b921fbe5ebbf66b663c6d570
Ben Everson <ben.everson@gmail.com>
2015-01-21 20:09:56 -0800

    added pickling to make init statements happen faster in model.py

--- app/describeer/describeer_config.py
+++ app/describeer/describeer_config.py
@@ -1,5 +1,13 @@
-DEFAULT_MODEL_PATH = 'ReviewIndividualSentencesMultiLabelSize300.doc2vec'
+DEFAULT_MODEL_PATH = 'ReviewIndividualSentencesMultiLabelDefault.doc2vec'
 DEFAULT_CSV_PATH = 'filtered_raw_reviews.csv'
+
+DEFAULT_PICKLE_PATH = 'pickles/'
+name_to_style_path = 'nametostyle.pickle'
+name_to_popularity_path = 'nametopopularity.pickle'
+name_to_rating_path = 'nametorating.pickle'
+style_list_path = 'stylelist.pickle'
+name_list_path = 'namelist.pickle'
+
 N_SEARCH_RESULTS = 50
 
 style_column = 5
@@ -10,4 +18,4 @@ overall_score_column = 8
 STYLE_SIM_WEIGHT = 2.0
 BEER_SIM_WEIGHT = 1.0
 BEER_POP_WEIGHT = 1.5
-BEER_RATING_WEIGHT = 1.5
+BEER_RATING_WEIGHT = 2.0
